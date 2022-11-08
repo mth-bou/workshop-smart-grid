399d5ebf6bbbaa54c8f859ac8b9657b4d609b069
Mathieu Boucher <mathieu.boucher@epsi.fr>
2022-11-08 15:24:27 +0100

    React Initialization and Add Sidebar

---
+++ src/component/sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    const [sidebar, setSidebar] = useState(false)
+
+    const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+        <>
+            <div className="navbar">
+                <Link to="#" className="menu-bars">
+                    <Icons.Menu sx={{ color: "white"}} onClick={showSidebar} />
+                </Link>
+                <h1>League Data</h1>
+            </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className="nav-menu-items" onClick={showSidebar}>
+                    <li className="navbar-toggle">
+                        <Link to="#" className="menu-bars">
+                            <Icons.Close sx={{ color: "white"}} />
+                        </Link>
+                    </li>
+                    {SidebarRoutes.map((item, index) => {
+                        return(
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.name}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Sidebar;
